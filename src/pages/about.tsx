4fcc8a6a2615836f8fbed7392c60582334ddb5e9
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-05 09:30:43 -0700

    updated about me content

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,77 @@
 import { type NextPage } from "next";
 
 import Head from "next/head";
+import Timeline, { type TimelineItem } from "~/components/custom/timeline";
+import H1 from "~/components/styled-tags/h1";
+import H2 from "~/components/styled-tags/h2";
 import Main from "~/components/styled-tags/main";
+
+const timelineItems: TimelineItem[] = [
+  {
+    eventType: "Life",
+    date: "April 1994",
+    description: "Beginning of existence.",
+  },
+
+  {
+    eventType: "Life",
+    date: "April 1994 - May 2012:",
+    description:
+      "Standard human progression from child to high school graduate.",
+  },
+  {
+    eventType: "School",
+    date: "August 2012",
+    description: " College begins.",
+  },
+  {
+    eventType: "Work",
+    date: "January 2015",
+    description: "Start working as a math tutor in the college library.",
+  },
+  {
+    eventType: "Work",
+    date: "January 2017",
+    description:
+      "First internship at Kennedy Space Center writing HTML and CSS websites.",
+  },
+  {
+    eventType: "School",
+    date: "August 2017",
+    description:
+      "Graduate college with degree in Computer Science. Hired full-time at Kennedy Space Center at the end of my internship.",
+  },
+  {
+    eventType: "Work",
+    date: "January 2018",
+    description: "Hired by Lockheed Martin as an Embedded Software Engineer.",
+  },
+  {
+    eventType: "Work",
+    date: "September 2020",
+    description: "First promotion.",
+  },
+  {
+    eventType: "Work",
+    date: "March 2021",
+    description: "Hired as a Software Engineer L4 at Google.",
+  },
+  {
+    eventType: "Life",
+    date: "August 2021",
+    description: "Moved to San Francisco.",
+  },
+  {
+    eventType: "Life and Work",
+    date: "March 2023",
+    description: "Left full-time employment.",
+  },
+  {
+    eventType: "Work",
+    date: "Present day",
+    description: "Working on my own small software business.",
+  },
+];
 
 const Home: NextPage = () => {
   return (
@@ -12,45 +82,35 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Main>
-        <h1>About</h1>
+        <H1>About</H1>
         <p>
-          I’m a professional software engineer. That just means that I’ve
-          written and read a lot of code over the years. I have a pretty good
-          idea of how computers work and am generally enthusiastic about
-          learning new things and build things with code. I have a deep interest
-          in the web and love building websites and applications which have a
-          tangible impact on real people. Seeing someone use something you’ve
-          built is a wonderful feeling.
+          Passionate Software Engineer | Building Impactful Web Applications
         </p>
-        <h2>A brief history of me:</h2>
-        <ul>
-          <li>April 1994: Beginning of existence.</li>
-          <li>
-            April 1994 - May 2012: Standard human progression from child to high
-            school graduate.
-          </li>
-          <li>August 2012: College begins.</li>
-          <li>
-            January 2015: Start working as a math tutor in the college library.
-          </li>
-          <li>
-            January 2017: First internship at Kennedy Space Center writing HTML
-            and CSS websites.
-          </li>
-          <li>
-            August 2017: Graduate college with degree in Computer Science. Hired
-            full-time at Kennedy Space Center at the end of my internship.
-          </li>
-          <li>
-            January 2018: Hired by Lockheed Martin as an Embedded Software
-            Engineer.
-          </li>
-          <li>September 2020: First promotion.</li>
-          <li>March 2021: Hired as a Software Engineer L4 at Google.</li>
-          <li>August 2021: Moved to San Francisco.</li>
-          <li>March 2023: Left full-time employment.</li>
-          <li>Present day: Working on my own small software business.</li>
-        </ul>
+        <p>
+          With years of experience as a professional software engineer,
+          I&apos;ve immersed myself in the world of coding, constantly expanding
+          my knowledge and skills. My deep interest in the web has fueled my
+          enthusiasm for creating web applications that make a tangible impact
+          on real people
+        </p>
+        <p>
+          Driven by a relentless curiosity, I thrive on learning and embracing
+          new technologies. My expertise lies in understanding how computers
+          work and leveraging that knowledge to build innovative solutions. I am
+          dedicated to crafting web experiences that empower businesses to
+          thrive in the digital realm.
+        </p>
+        <p>
+          From revamping website designs to optimizing performance and
+          developing robust e-commerce platforms, I bring creativity and
+          technical proficiency to every project. By collaborating closely with
+          clients and tailoring solutions to their unique needs, I ensure
+          exceptional quality and exceed expectations.
+        </p>
+        <p>
+          Join me in leveraging the full potential of the web to transform your
+          online presence. Let&apos;s create something remarkable together.
+        </p>
       </Main>
     </>
   );
