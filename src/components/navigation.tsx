449edcf71aeb0697bd7223339cca76bc563716d5
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-06 12:21:44 -0700

    add shadows to buttons

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -7,20 +7,26 @@ import {
 } from "~/components/ui/dropdown-menu";
 import Image from "next/image";
 import Link from "next/link";
+import { ChevronDown } from "lucide-react";
 
 export default function Navigation() {
   return (
     <div className="flex justify-between px-8 pt-8">
-      <Image
-        alt="hunter heston's profile pic."
-        src="/hunter.jpeg"
-        className="h-12 w-12 rounded-full"
-        width={75}
-        height={75}
-      ></Image>
+      <Link href="/">
+        <Image
+          alt="hunter heston's profile pic."
+          src="/hunter.jpeg"
+          className="h-12 w-12 rounded-full"
+          width={75}
+          height={75}
+        ></Image>
+      </Link>
       <div className="flex items-center justify-center gap-4">
         <DropdownMenu>
-          <DropdownMenuTrigger>Menu</DropdownMenuTrigger>
+          <DropdownMenuTrigger className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
+            <p>Menu</p>
+            <ChevronDown className="h-4 w-4"></ChevronDown>
+          </DropdownMenuTrigger>
           <DropdownMenuContent>
             <DropdownMenuItem>
               <Link href="/services">Service</Link>
@@ -43,7 +49,9 @@ export default function Navigation() {
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-        <div>☀️</div>
+        <div className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20">
+          ☀️
+        </div>
       </div>
     </div>
   );
