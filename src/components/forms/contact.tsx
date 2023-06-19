cc8184538240bdeaec1d1b56dd98d4311d496490
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-19 10:34:36 -0700

    Form works

--- src/components/forms/contact.tsx
+++ src/components/forms/contact.tsx
@@ -11,11 +11,18 @@ import {
 } from "../ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
 import { useForm } from "react-hook-form";
 
 const formSchema = z.object({
   fullname: z.string().min(1, {
     message: "Full name must not be empty.",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  message: z.string().min(1, {
+    message: "Message must not be empty.",
   }),
 });
 
@@ -25,6 +32,8 @@ export function ContactForm() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       fullname: "",
+      email: "",
+      message: "",
     },
   });
 
@@ -48,6 +57,34 @@ export function ContactForm() {
                 <Input placeholder="" {...field} />
               </FormControl>
               <FormDescription>Your full name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="example@gmail.com" {...field} />
+              </FormControl>
+              <FormDescription>The best email to reach out at.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Message</FormLabel>
+              <FormControl>
+                <Textarea placeholder="" {...field} />
+              </FormControl>
+              <FormDescription>How can I help you?</FormDescription>
               <FormMessage />
             </FormItem>
           )}
