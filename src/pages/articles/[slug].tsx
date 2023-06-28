78855e6369cc02295d2cdbaff697ada80a39c76a
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-28 14:29:18 -0700

    Typography for articles.

--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -2,6 +2,28 @@ import type { GetStaticProps, GetStaticPropsContext } from "next";
 import Head from "next/head";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import {
+  H1,
+  H2,
+  H3,
+  H4,
+  Blockquote,
+  InlineCode,
+  Large,
+  Lead,
+  Ul,
+  Li,
+  Ol,
+  Muted,
+  P,
+  Small,
+  THead,
+  TBody,
+  Table,
+  Th,
+  Td,
+  Tr,
+} from "@/components/ui/typography";
+import {
   type BlogArticle,
   getBlogArticles,
   lookupBlogArticle,
@@ -21,12 +43,68 @@ export default function Article({ article }: ArticleProps) {
         {/* <meta name="description" content={article.description} /> */}
         <meta name="date" content={datestringToYearMonthDay(article.created)} />
       </Head>
-      <main className="prose px-5 pt-16">
-        <h1>{article.title}</h1>
-        <p className="border-l-2 border-l-zinc-600 pl-2 align-middle text-zinc-600">
-          {dateStringToDayMonthYear(article.created)}
-        </p>
-        <ReactMarkdown>{article.markdown}</ReactMarkdown>
+      <main className="flex justify-center">
+        <div className="prose px-5 pt-16 text-primary">
+          <h1 className="text-inherit">{article.title}</h1>
+          <p className="border-l-2 border-l-zinc-600 pl-2 align-middle text-zinc-600 dark:text-primary">
+            {dateStringToDayMonthYear(article.created)}
+          </p>
+          <ReactMarkdown
+            components={{
+              h1: ({ node, ...props }) => <H1 {...props} />,
+              h2: ({ node, ...props }) => <H2 {...props} />,
+              h3: ({ node, ...props }) => <H3 {...props} />,
+              h4: ({ node, ...props }) => <H4 {...props} />,
+              h5: ({ node, ...props }) => <h5 className="mb-2" {...props} />,
+              h6: ({ node, ...props }) => <h6 className="mb-2" {...props} />,
+              p: ({ node, ...props }) => (
+                <P className="my-5 leading-loose" {...props} />
+              ),
+              a: ({ node, ...props }) => (
+                <a
+                  className="text-primary transition-all hover:text-zinc-600"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  {...props}
+                />
+              ),
+              blockquote: ({ node, ...props }) => <Blockquote {...props} />,
+              br: ({ node, ...props }) => <br {...props} />,
+              em: ({ node, ...props }) => <em {...props} />,
+              hr: ({ node, ...props }) => <hr className="my-4" {...props} />,
+              img: ({ node, ...props }) => (
+                // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
+                <img {...props} />
+              ),
+              pre: ({ node, ...props }) => <pre className="" {...props} />,
+              strong: ({ node, ...props }) => <strong {...props} />,
+              ol: ({ node, ...props }) => <Ol {...props} />,
+              ul: ({ node, ...props }) => <Ul {...props} />,
+              li: ({ node, ...props }) => <Li {...props} />,
+              code: ({ node, inline, className, children, ...props }) =>
+                inline ? (
+                  <InlineCode {...props}>{children}</InlineCode>
+                ) : (
+                  <code className={className} {...props}>
+                    {String(children)}
+                  </code>
+                ),
+              table: ({ node, ...props }) => <Table {...props} />,
+              thead: ({ node, ...props }) => <THead {...props} />,
+              tbody: ({ node, ...props }) => <tbody {...props} />,
+              tr: ({ node, ...props }) => (
+                <tr
+                  className="even:dark:text-darkAccent even:text-accent"
+                  {...props}
+                />
+              ),
+              th: ({ node, ...props }) => <Th {...props} />,
+              td: ({ node, ...props }) => <Td {...props} />,
+            }}
+          >
+            {article.markdown}
+          </ReactMarkdown>
+        </div>
       </main>
     </>
   );
