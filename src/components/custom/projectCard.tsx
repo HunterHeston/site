bf6621474c74378e00be942cf875213fa185fda8
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-28 14:45:17 -0700

    add dark shadow

--- src/components/custom/projectCard.tsx
+++ src/components/custom/projectCard.tsx
@@ -29,7 +29,7 @@ export default function ProjectCard({
     <Card className="transition-all hover:-translate-y-1 hover:bg-zinc-50 dark:hover:bg-zinc-800">
       <CardHeader>
         <div className="flex gap-2">
-          <Avatar className="h-8 w-8 shadow-md shadow-neutral-400 ">
+          <Avatar className="h-8 w-8 shadow-md shadow-neutral-400 dark:shadow-zinc-800">
             <AvatarImage src={imgPath} alt="logo"></AvatarImage>
             <AvatarFallback>{fallback}</AvatarFallback>
           </Avatar>
