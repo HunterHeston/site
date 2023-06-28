8c02955d0b2a757c88d69cda10e3fb41ce32e15b
Hunter Heston <huntersamuelheston@gmail.com>
2023-06-28 08:54:54 -0700

    Changing emojis on darkmode switch.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
 import { useState, type ReactNode } from "react";
 import Navigation from "./navigation";
+import { ThemeProvider } from "./themeProvider";
 
 enum Theme {
   LIGHT = "",
@@ -23,7 +24,7 @@ const Layout = ({ children }: Props) => {
               }
               className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20"
             >
-              ☀️
+              {darkMode === Theme.LIGHT ? "🌙" : "☀️"}
             </button>
           </Navigation>
           <div>{children}</div>
