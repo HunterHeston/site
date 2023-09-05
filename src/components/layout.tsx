61b6e9d2bfad54c5d46435cf996799f9bbefd67d
Hunter Heston <huntersamuelheston@gmail.com>
2023-09-05 16:16:02 -0700

    fix footer a screen bottom

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -28,21 +28,24 @@ const Layout = ({ children }: Props) => {
   return (
     <div>
       <div className="flex min-h-screen w-screen flex-col items-center bg-zinc-50 duration-300 dark:bg-zinc-950">
-        <div className="min-h-screen w-screen bg-white duration-300 dark:bg-zinc-900 dark:text-white md:w-5/6 md:px-5">
-          <Navigation>
-            <button
-              onClick={() => {
-                const nextTheme =
-                  darkMode === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
-                setDarkMode(nextTheme);
-                setDarkModeOnBody(nextTheme);
-              }}
-              className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20"
-            >
-              {darkMode === Theme.LIGHT ? "🌙" : "☀️"}
-            </button>
-          </Navigation>
-          <div>{children}</div>
+        <div className="flex min-h-screen w-screen flex-col bg-white duration-300 dark:bg-zinc-900 dark:text-white md:w-5/6 md:px-5">
+          {/* container that pushes footer to the bottom on large screens */}
+          <div className="flex-grow">
+            <Navigation>
+              <button
+                onClick={() => {
+                  const nextTheme =
+                    darkMode === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+                  setDarkMode(nextTheme);
+                  setDarkModeOnBody(nextTheme);
+                }}
+                className="group flex items-center rounded-full bg-white/90 px-4 py-2 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10 dark:hover:ring-white/20"
+              >
+                {darkMode === Theme.LIGHT ? "🌙" : "☀️"}
+              </button>
+            </Navigation>
+            <div>{children}</div>
+          </div>
           <Footer></Footer>
         </div>
       </div>
