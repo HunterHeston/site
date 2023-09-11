ba9ab139a4766ba1e678c907bf8c66a22c84d8e6
Hunter Heston <huntersamuelheston@gmail.com>
2023-09-11 11:15:03 -0700

    Update duration

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -27,8 +27,8 @@ const Layout = ({ children }: Props) => {
 
   return (
     <div>
-      <div className="flex min-h-screen w-screen flex-col items-center bg-zinc-50 duration-300 dark:bg-zinc-950">
-        <div className="flex min-h-screen w-screen flex-col bg-white duration-300 dark:bg-zinc-900 dark:text-white md:w-5/6 md:px-5">
+      <div className="flex min-h-screen w-screen flex-col items-center bg-zinc-50 duration-100 dark:bg-zinc-950">
+        <div className="flex min-h-screen w-screen flex-col bg-white duration-100 dark:bg-zinc-900 dark:text-white md:w-5/6 md:px-5">
           {/* container that pushes footer to the bottom on large screens */}
           <div className="flex-grow">
             <Navigation>
@@ -36,7 +36,7 @@ const Layout = ({ children }: Props) => {
                 target="_blank"
                 href="/hunter-heston.pdf"
                 className={
-                  "group flex items-center rounded-full bg-gradient-to-r from-blue-500 to-purple-600 px-4 py-2 text-sm font-medium  transition duration-300 hover:from-blue-600 hover:to-purple-800 "
+                  "group flex items-center rounded-full bg-gradient-to-r from-blue-500 to-purple-600 px-4 py-2 text-sm font-medium  hover:from-blue-600 hover:to-purple-800 "
                 }
               >
                 Resume
