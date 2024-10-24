0a2d685d5c7b21fdbbd4acde8cbc2650256ce96d
Hunter Heston <huntersamuelheston@gmail.com>
2024-10-23 21:02:13 -0700

    pnpm i @vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import Layout from "~/components/layout";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Layout>
         <Component {...pageProps} />
+        <Analytics />
       </Layout>
     </>
   );
